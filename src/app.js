0020e81c0f99179c1f72ae274515c2efb765d307
ienix <guoaimin01@baidu.com>
2018-07-12 14:15:14 +0800

    add push log

--- src/app.js
+++ src/app.js
@@ -17,37 +17,43 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
 
-app.post('/reciver', (request, response) => {
-    request.setEncoding('utf8');
-    request.on('data', data => {
-        let result = null;
+app
+    .post('/reciver', (request, response) => {
+        request.setEncoding('utf8');
+        request.on('data', data => {
+            let result = null;
 
-        try {
-            result = JSON.parse(data);
-        }
-        catch (e) {
-            console.log(e, '`Parse JSON error`');
-            return response.sendStatus(500);
-        }
-
-        try {
-            let {content = '', path = '', resovlePath = ''} = result;
-            let dirPath = PATH.dirname(resovlePath);
-
-            if (!FS.existsSync(dirPath)) {
-                FSE.ensureDirSync(dirPath);
+            try {
+                result = JSON.parse(data);
+            }
+            catch (e) {
+                console.log(e, '`Parse JSON error`');
+                return response.sendStatus(500);
             }
 
-            FS.writeFileSync(resovlePath, content, {encoding: 'utf8', flag: 'w'});
-        }
-        catch(e) {
-            console.log(e, '`Write file error`');
-            return response.sendStatus(500);
-        }
+            try {
+                let {content = '', path = '', resovlePath = ''} = result;
+                let dirPath = PATH.dirname(resovlePath);
 
-        response.sendStatus(200);
+                if (!FS.existsSync(dirPath)) {
+                    FSE.ensureDirSync(dirPath);
+                }
+
+                FS.writeFileSync(resovlePath, content, {encoding: 'utf8', flag: 'w'});
+
+                console.log('[OK] - The `%s` write success!', resovlePath);
+            }
+            catch(e) {
+                console.log(e, '`Write file error`');
+                return response.sendStatus(500);
+            }
+
+            response.sendStatus(200);
+        });
+    })
+    .get('/reciver', (request, response) => {
+        response.send('hello world!')
     });
-});
 
 app.listen(8899, () => {  
     console.log('Example app listening at 8899');
